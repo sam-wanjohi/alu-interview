ae3705addb5695bf990742ace4216f434b2caf8b
Samuel Wanjohi <s.wanjohi@alustudent.com>
2024-02-24 17:53:59 +0000

    file creation

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Returns list of integers representing
+Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """Returns empty list if n >= 0"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
